1616fbd65558b428f2bd97b652e1137be3c8d2cf
ShawnZhong <shawnzhongchn@gmail.com>
2025-09-17 10:05:22 +0800

    plugins/add_plugins_0917
    add_plugins:
    1. blink
    2. lsp_config
    3. dap_python

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,7 +1,14 @@
+-- 配置 Python 与 TOML LSP (pyright, taplo, ruff) 并统一 capabilities
 return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts) ---@class PluginLspOpts
+      -- 生成基础 capabilities，支持 snippet 补全
+      local base_capabilities =
+        vim.tbl_deep_extend("force", opts.capabilities or {}, vim.lsp.protocol.make_client_capabilities())
+      base_capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      -- 合并 LSP 配置
       local ret = vim.tbl_deep_extend("force", opts, {
         servers = {
           pyright = {
@@ -13,7 +20,7 @@ return {
                   autoSearchPaths = true,
                   diagnosticMode = "openFiles",
                   diagnosticSeverityOverrides = {
-                    reportArgumentType = "none", -- ✅ 这里才对
+                    reportArgumentType = "none",
                     reportGeneralTypeIssues = "none",
                     reportOptionalMemberAccess = "none",
                     reportOptionalSubscript = "none",
@@ -23,7 +30,18 @@ return {
                 },
               },
             },
+            capabilities = base_capabilities,
             mason = false,
+          },
+          taplo = {
+            capabilities = base_capabilities,
+          },
+          ruff = {
+            capabilities = base_capabilities,
+            -- 禁用 hover 避免与 pyright 冲突
+            on_attach = function(client)
+              client.server_capabilities.hoverProvider = false
+            end,
           },
         },
         capabilities = vim.tbl_deep_extend("force", opts.capabilities or {}, {
