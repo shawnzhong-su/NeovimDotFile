c30870dd043f732565ea48d9b7b5d6a59fd93d95
ShawnZhong <shawnzhongchn@gmail.com>
2025-09-22 10:04:10 +0800

    修改 Depy

--- lua/plugins/nvim-dap-python.lua
+++ lua/plugins/nvim-dap-python.lua
@@ -67,12 +67,8 @@ return {
     "mfussenegger/nvim-dap-python",
     dependencies = "mfussenegger/nvim-dap",
     config = function()
-      -- fix: E5108: Error executing lua .../Local/nvim-data/lazy/nvim-dap-ui/lua/dapui/controls.lua:14: attempt to index local 'element' (a nil value)
-      -- see: https://github.com/rcarriga/nvim-dap-ui/issues/279#issuecomment-1596258077
       require("dapui").setup()
-      -- uses the debugypy installation by mason
-      local debugpyPythonPath = require("mason-registry").get_package("debugpy"):get_install_path()
-        .. "/venv/bin/python3"
+      local debugpyPythonPath = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python3"
       require("dap-python").setup(debugpyPythonPath, {}) ---@diagnostic disable-line: missing-fields
     end,
   },
